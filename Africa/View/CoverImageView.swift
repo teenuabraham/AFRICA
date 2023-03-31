ba512fb6bb4a51b6829265448174585ca30b69d1
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-03-31 22:39:59 +0200

    CoverImageView Added

---
+++ Africa/View/CoverImageView.swift
@@ -0,0 +1,32 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Teenu Abraham on 31/03/23.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        TabView {
+            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Image("cover-lion")
+                      .resizable()
+                  .scaledToFit()
+            } //: Loop
+        } //: Tab View
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+// MARK: - PREVIEW
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}
