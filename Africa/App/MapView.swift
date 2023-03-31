0cd09b459e1722f7f0e0c853a332c37a9677c1a4
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-03-31 15:38:15 +0200

    Add Tab Bar (Content, Map. Gallery,Map)

---
+++ Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Teenu Abraham on 31/03/23.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Teenu Abraham on 31/03/23.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
