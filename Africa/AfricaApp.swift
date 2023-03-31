4889cc00529d915c6c4947ab754e9c68c82ce74c
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-03-31 14:33:46 +0200

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Teenu Abraham on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
