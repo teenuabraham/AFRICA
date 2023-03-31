36a5774ac99a59edc529e36645fcd7f063310bd0
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-03-31 22:44:58 +0200

    Implemented CoverImageDataModel

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Teenu Abraham on 31/03/23.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable{
+    let id: Int
+    let name: String
+}
