6818fb2bce4bd5ab0b81c13b466309b17a61ca34
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-04-13 22:24:16 +0200

    Implentented Video List

---
+++ Africa/View/VideoListItemView.swift
@@ -0,0 +1,52 @@
+//
+//  VideoListItem.swift
+//  Africa
+//
+//  Created by Teenu Abraham on 13/04/23.
+//
+
+import SwiftUI
+
+struct VideoListItemView: View {
+    // MARK: - PROPERTIES
+    let video: Video
+    // MARK: - BODY
+    var body: some View {
+        HStack(spacing: 10) {
+            ZStack {
+                Image(video.thumbnail)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                    .cornerRadius(9)
+                
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 4)
+            } //: Zstack
+            VStack(alignment:.leading, spacing: 10) {
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(video.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+            }
+        } //: Hstack
+    }
+}
+
+// MARK: - PREVIEW
+struct VideoListItem_Previews: PreviewProvider {
+    static let videos:[Video] = Bundle.main.decode("videos.json")
+    static var previews: some View {
+        VideoListItemView(video: videos[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
