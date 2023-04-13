9a9473e890b87f5dde4dac847155f737a3915c0d
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-04-13 22:32:15 +0200

    VideoPlayer

---
+++ Africa/View/VideoPlayerView.swift
@@ -0,0 +1,25 @@
+//
+//  VideoPlayerView.swift
+//  Africa
+//
+//  Created by Teenu Abraham on 13/04/23.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "cheetah", withExtension: "mp4")!))
+    }
+}
+
+// MARK: - PREVIEW
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoPlayerView()
+    }
+}
