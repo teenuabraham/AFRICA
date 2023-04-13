97b860e8a93498f2390b00b19342dc526f3583be
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-04-13 21:52:32 +0200

    VideoModel

---
+++ Africa/Model/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Teenu Abraham on 13/04/23.
+//
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+}
